ba0d986080b9f7f38ca9085a0617dd7ecc3097ec
burgerkiss25 <burgerkiss25@gmail.com>
2026-01-09 01:46:31 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   SUPABASE_URL: window.SUPABASE_URL || "https://iafhmqsktegezwwjgkzz.supabase.co",
   SUPABASE_ANON_KEY: window.SUPABASE_ANON_KEY || "DEIN_SB_PUBLISHABLE_KEY_HIER",
-  BASIS_UNIT_PRICE: 6,
+  BASIS_UNIT_PRICE: 6,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhZmhtcXNrdGVnZXp3d2pna3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MDk4NjEsImV4cCI6MjA4MzQ4NTg2MX0.CGbpBGD60WeJmQ2OHl_XkKZteLvz13czdQWr4tM0v8E
 };
 
 export const supabaseClient = () => {
