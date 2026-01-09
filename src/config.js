0129eabc2861e56886b9328b07ed82d01263164b
burgerkiss25 <burgerkiss25@gmail.com>
2026-01-09 01:58:29 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   SUPABASE_URL: window.SUPABASE_URL || "https://iafhmqsktegezwwjgkzz.supabase.co",
-  SUPABASE_ANON_KEY: window.SUPABASE_ANON_KEY || "sb_publishable_SRL1qRkPGfGdpGO2wn07TA_939_NCo4",
+  SUPABASE_ANON_KEY: window.SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhZmhtcXNrdGVnZXp3d2pna3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MDk4NjEsImV4cCI6MjA4MzQ4NTg2MX0.CGbpBGD60WeJmQ2OHl_XkKZteLvz13czdQWr4tM0v8E",
   BASIS_UNIT_PRICE: 6,
 };
 
