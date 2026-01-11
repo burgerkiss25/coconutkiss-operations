fd482f4d8d57daa7b12c226f5f2baf6c0f283bfd
burgerkiss25 <burgerkiss25@gmail.com>
2026-01-11 23:48:40 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const CONFIG = {
   // WICHTIG: Trage hier deine echten Werte ein (so wie bisher in deinem Projekt)
-  SUPABASE_URL: window.SUPABASE_URL || "",
-  SUPABASE_ANON_KEY: window.SUPABASE_ANON_KEY || "",
+  SUPABASE_URL: window.SUPABASE_URL || "https://iafhmqsktegezwwjgkzz.supabase.co",
+  SUPABASE_ANON_KEY: window.SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhZmhtcXNrdGVnZXp3d2pna3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MDk4NjEsImV4cCI6MjA4MzQ4NTg2MX0.CGbpBGD60WeJmQ2OHl_XkKZteLvz13czdQWr4tM0v8E",
 
   // falls du es in db.js nutzt
   BASIS_UNIT_PRICE: 6,
